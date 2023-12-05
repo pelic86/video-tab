e81811410f838bb4d7fc511bd1945a677eb8e6b9
Pelic <pelicwei@gmail.com>
2023-12-05 23:54:00 +0800

    refactor: move fetch game data into custom hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,38 +1,24 @@
-import { useEffect, useState, Fragment } from "react";
-import { Text } from "@chakra-ui/react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  slug: string;
-  name: string;
-  background_image: string;
-}
-
-interface FetchGamesResponese {
-  count: number;
-  results: Game[];
-}
+import { Fragment } from "react";
+import { Spinner, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponese>("/games")
-      .then(res => setGames(res.data.results))
-      .catch(err => setError(err.message));
-  }, []);
+  const { games, error, isLoading } = useGames();
 
   return (
     <Fragment>
-      {error && <Text>{error}</Text>}
-      <ul>
-        {games.map(game => (
-          <li key={game.id}>{game.id}</li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          {error && <Text>{error}</Text>}
+          <ul>
+            {games.map(game => (
+              <li key={game.id}>{game.id}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </Fragment>
   );
 };
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  slug: string;
+  name: string;
+  background_image: string;
+}
+
+interface FetchGamesResponese {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+
+    apiClient
+      .get<FetchGamesResponese>("/games", { signal: controller.signal })
+      .then(res => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
+      .catch(err => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error, isLoading };
+};
+
+export default useGames;
