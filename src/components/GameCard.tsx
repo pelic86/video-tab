c8b75d9526532d089027d31c62a82fa17c13bae1
Pelic <pelicwei@gmail.com>
2023-12-20 23:54:13 +0800

    feat: add score badge

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Img } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Img } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -24,10 +25,12 @@ const GameCard = ({ game }: Props) => {
       />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
-
-        <PlatformIconList
-          platforms={game.parent_platforms.map(({ platform }) => platform)}
-        />
+        <HStack justifyContent={"space-between"}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map(({ platform }) => platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
